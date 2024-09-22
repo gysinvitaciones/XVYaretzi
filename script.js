a92924661e4d59c022dd5dee1251db4cf155bd0e
gysinvitaciones <goooosss11@gmail.com>
2024-09-22 11:25:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const guests = document.getElementById('guests').value;
 
         // URL de WhatsApp con el mensaje predefinido
-        const whatsappURL = `https://api.whatsapp.com/send?phone=+528112289954&text=Hola,%20somos%20${familyName}%20y%20asistiremos%20al%20evento%20con%20${guests}%20invitado(s).`;
-
-        // Abrir enlace de WhatsApp en una nueva pestaña
-        window.open(whatsappURL, '_blank');
+    
     });
 
 
